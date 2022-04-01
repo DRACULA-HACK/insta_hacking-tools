03c6a2a32da8aefb93bf793566d48cdbd0dcc7e6
Master-hack <96709855+DRACULA-HACK@users.noreply.github.com>
2022-04-01 12:45:08 +0530

    Update insta-hacking-tools.sh

--- insta-hacking-tools.sh
+++ insta-hacking-tools.sh
@@ -17,7 +17,7 @@ apt-get install python
 apt-get install python2
 clear
 echo -e "$Purple Created By \e[1;34m"
-       figlet Master-hack | lolcat
+       figlet figlet Incredible Hacker | lolcat
 sleep 2.0
     echo -e " \e[96m
                                      ████████ \e[0m"
